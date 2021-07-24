6feaa3677719fbd09c37462370b18288a7a86833
Ntunyu Serge <ntunyuserge@gmail.com>
2021-07-24 17:05:57 +0100

    Deleted library that was imported before
    Previously, there was a slight error. The array to be returned has elements which are in the original array but the difference is that the resulting array has distinct elements whereas the previous code was generating random numbers which were different from the one in the original string

--- Exercise_27.py
+++ Exercise_27.py
@@ -1,15 +1,13 @@
 # Create a function that will receive an array of numbers as argument and will return a
 # new array with distinct elements
-import random
 def array_of_distinct_elements(array):
     result = []
-    for i in range(len(array)):
-        while len(result) != len(array):
-            elt = random.randint(min(array), max(array)*2)
-            if elt in array or elt in result:
-                continue
+    for elt in array:
+        if elt not in result:
             result.append(elt)
     return result
 
-print(array_of_distinct_elements([1,2,3,4,5]))
+
+print(array_of_distinct_elements([1,2,1,4,3,4,5]))
+
 
