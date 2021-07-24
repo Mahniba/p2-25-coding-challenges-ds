8ee9cb8d2fe18dd33cd60eae0f3d1e82da9171f0
Ntunyu Serge <ntunyuserge@gmail.com>
2021-07-24 19:10:34 +0100

    List comprehension implemented
    List comprehension eliminated the three lines of code which had the for loop, if statement and append function.

--- Exercise_26.py
+++ Exercise_26.py
@@ -2,9 +2,7 @@
 # elements that are in the first array but not in the second
 def elements_in_first_array_only(arr1, arr2):
     result = []
-    for elt in arr1:
-        if elt not in arr2:
-            result.append(elt)
-    return result
+    return [elt for elt in arr1 if elt not in arr2]
+    
 
 print(elements_in_first_array_only([1,4,5,3], [1,5,7,9,0]))
