73a7f1b6e24d92fe22c1190fdb0abff790798f73
Ntunyu Serge <ntunyuserge@gmail.com>
2021-07-24 19:33:11 +0100

    finding number of words in a text using len() and split()

---
+++ Exercise_31.py
@@ -0,0 +1,7 @@
+#  Create a function
+# that will return the number of words in a text
+
+def num_of_words(text):
+    return len(text.split())
+
+print(num_of_words("Create a function that will return the number words"))
