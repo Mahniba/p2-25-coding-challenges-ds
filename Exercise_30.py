0cbbaf7fc751627a8836c3b6355edead6f5e7afb
Ntunyu Serge <ntunyuserge@gmail.com>
2021-07-28 13:22:08 +0100

    Eliminated the exception handling that was used
    The difference in the number of digits is used to make up both numbers to equal sized by adding zeros to the left of the the shortest number

--- Exercise_30.py
+++ Exercise_30.py
@@ -4,16 +4,12 @@ def add(num1, num2):
     result = ""
     carry = 0
     if len(num1) > len(num2):
-        longest = num1
+        num2 = "0"*(len(num1) - len(num2)) + num2
     else:
-        longest = num2
+        num1 = "0"*(len(num2)-len(num1)) + num1
     for i in range(1, (max(len(num1), len(num2)))+1):
-        try:
-            result = str(((int(num1[-i]) + int(num2[-i])) + carry) % 10) + result
-            carry = (int(num1[-i]) + int(num2[-i]) + carry) // 10
-        except IndexError:
-            result = str(((int(longest[-i])) + carry) % 10) + result
-            carry = ((int(longest[-i])) + carry) // 10
+        result = str(((int(num1[-i]) + int(num2[-i])) + carry) % 10) + result
+        carry = (int(num1[-i]) + int(num2[-i]) + carry) // 10
     if carry == 0:
         return result
     else:
