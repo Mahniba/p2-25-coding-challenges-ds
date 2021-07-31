ee1202af15bc535c1f236184a21474b20c040a98
alomba epong <alombangwingche@gmail.com>
2021-07-31 20:14:07 +0100

    Calculate the sum of first 100 prime numbers and return them in an array

---
+++ vanilla/ex28.py
@@ -0,0 +1,27 @@
+def isPrime(y):
+    flag = False
+    if y == 1:
+        flag = False
+
+    for i in range(2,y):
+        if y % i == 0:
+           flag = False
+        else:
+            flag = True
+        return flag
+y=int(input("enter a number: "))
+print(isPrime(y))
+
+def sum(m):
+    sum = 0
+    i = 1
+    counter = 0
+    array = []
+    while counter > m:
+        if isPrime(i):
+            array.append(i)
+            sum = sum + 1
+            counter += 1
+        i += 1
+        return sum, array
+print(sum(3))
