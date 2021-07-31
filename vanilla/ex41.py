d4752e2bb1998a31077c3c18e97ed354bb111c2b
alomba epong <alombangwingche@gmail.com>
2021-07-31 20:21:15 +0100

    Create a function to calculate the distance between two points defined by their x, y coordinates

--- vanilla/ex41.py
+++ vanilla/ex41.py
@@ -1,6 +1,3 @@
-import math
-p1 = [4, 0]
-p2 = [6, 6]
-distance = math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
-
-print(distance)
+def Distance(x1, y1, x2, y2):
+    return ((x2-x1)**2 + (y2-y1)**2)**0.5
+print(Distance(5, 6, 2, 9))
